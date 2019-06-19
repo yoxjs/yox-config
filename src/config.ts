519f0f4773ecb2bf918c707f8c99b90c812a8b0b
musicode <musicode@qq.com>
2019-06-19 22:27:11 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -23,6 +23,8 @@ export const DIRECTIVE_CUSTOM = 'o'
 
 export const MODEL_PROP_DEFAULT = 'value'
 
+export const NAMESPACE_HOOK = '.hook'
+
 export const HOOK_BEFORE_CREATE = 'beforeCreate'
 export const HOOK_AFTER_CREATE = 'afterCreate'
 export const HOOK_BEFORE_MOUNT = 'beforeMount'
